8490955f7ef6a767bf93c6181df51b94ea213497
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-03-05 16:28:48 +0200

    fix, modal is opening

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import css from './Modal.module.css';
 
 export const Modal = ({ src, alt }) => {
-  <div className={css.overlay}>
+  return <div className={css.overlay}>
     <div className={css.modal}>
       <img src={src} alt={alt} />
     </div>
